8570e636e6ca65f49b5fd5c40aee9425daaabf05
Jeff Hobbs <jshobbssocal@gmail.com>
2026-07-12 16:14:40 -0500

    Configure application and add initial REST endpoint

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,11 +2,8 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class}
-)
+@SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
